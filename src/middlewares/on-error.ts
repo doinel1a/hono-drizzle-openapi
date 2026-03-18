be51d9ba631dde68d1b94ef213dbe42435a1dc70
doinel1a <doinel1atanasiu@gmail.com>
2026-03-18 19:20:01 +0100

    chore: on error middleware

---
+++ src/middlewares/on-error.ts
@@ -0,0 +1,26 @@
+/* eslint-disable unicorn/no-null */
+
+import type { ErrorHandler } from 'hono';
+import type { ContentfulStatusCode } from 'hono/utils/http-status';
+
+import { env } from '@/env';
+import { INTERNAL_SERVER_ERROR_CODE, OK_CODE } from '@/lib/constants/http-status-codes';
+
+const onError: ErrorHandler = (error, c) => {
+  const environment = c.env?.NODE_ENV ?? env.NODE_ENV;
+  const currentStatus = 'status' in error ? error.status : c.newResponse(null).status;
+  const statusCode =
+    currentStatus === OK_CODE
+      ? INTERNAL_SERVER_ERROR_CODE
+      : (currentStatus as ContentfulStatusCode);
+
+  return c.json(
+    {
+      message: error.message,
+      stack: environment === 'production' ? undefined : error.stack
+    },
+    statusCode
+  );
+};
+
+export default onError;
