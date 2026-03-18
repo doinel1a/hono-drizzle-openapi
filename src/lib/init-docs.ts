cee8f28176fd5235a29418881a4a6d5c6b8555a4
doinel1a <doinel1atanasiu@gmail.com>
2026-03-18 20:54:33 +0100

    chore: ui docs

--- src/lib/init-docs.ts
+++ src/lib/init-docs.ts
@@ -1,11 +1,25 @@
 import type { OpenAPIHono } from '@hono/zod-openapi';
 
+import { Scalar } from '@scalar/hono-api-reference';
+
 export default function initDocs(server: OpenAPIHono) {
-  return server.doc('/docs', {
+  server.doc('/docs', {
     openapi: '3.0.0',
     info: {
       title: 'API Documentation',
       version: '1.0.0'
     }
   });
+
+  server.get(
+    '/reference',
+    Scalar({
+      url: '/docs',
+      theme: 'kepler',
+      defaultHttpClient: {
+        clientKey: 'fetch',
+        targetKey: 'js'
+      }
+    })
+  );
 }
