a80a5f49d1c56307f01f3a36c69746bf44df64db
doinel1a <doinel1atanasiu@gmail.com>
2026-03-18 19:13:30 +0100

    chore: not found middleware

--- src/sever.ts
+++ src/sever.ts
@@ -1,6 +1,9 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 
+import notFound from './middlewares/not-found';
+
 const server = new OpenAPIHono();
+server.notFound(notFound);
 server.get('/', (c) => c.text('Hello, World!'));
 
 export default server;
