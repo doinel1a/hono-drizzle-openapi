32342d323aae6d13c19cbff0797da48db92a0453
doinel1a <doinel1atanasiu@gmail.com>
2026-03-18 19:34:16 +0100

    chore: logger middleware

--- src/middlewares/on-error.ts
+++ src/middlewares/on-error.ts
@@ -5,6 +5,7 @@ import type { ContentfulStatusCode } from 'hono/utils/http-status';
 
 import { env } from '@/env';
 import { INTERNAL_SERVER_ERROR_CODE, OK_CODE } from '@/lib/constants/http-status-codes';
+import { INTERNAL_SERVER_ERROR_PHRASE } from '@/lib/constants/http-status-phrases';
 
 const onError: ErrorHandler = (error, c) => {
   const environment = c.env?.NODE_ENV ?? env.NODE_ENV;
@@ -13,6 +14,8 @@ const onError: ErrorHandler = (error, c) => {
     currentStatus === OK_CODE
       ? INTERNAL_SERVER_ERROR_CODE
       : (currentStatus as ContentfulStatusCode);
+
+  c.var.logger.withError(error).error(INTERNAL_SERVER_ERROR_PHRASE);
 
   return c.json(
     {
--- src/sever.ts
+++ src/sever.ts
@@ -1,11 +1,23 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
+import { honoLogLayer } from '@loglayer/hono';
 
+import logger from './middlewares/logger';
 import notFound from './middlewares/not-found';
 import onError from './middlewares/on-error';
 
 const server = new OpenAPIHono();
+server.use(
+  honoLogLayer({
+    instance: logger,
+    autoLogging: {
+      request: { logLevel: 'debug' },
+      response: { logLevel: 'info' }
+    }
+  })
+);
 server.notFound(notFound);
 server.onError(onError);
+
 server.get('/', (c) => c.text('Hello, World!'));
 
 export default server;
