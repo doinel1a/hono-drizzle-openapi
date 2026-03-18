85482138c1b624fd8fa1b797520c0bf82ea5bb90
doinel1a <doinel1atanasiu@gmail.com>
2026-03-18 19:20:01 +0100

    chore: on error middleware

--- src/sever.ts
+++ src/sever.ts
@@ -1,9 +1,11 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 
 import notFound from './middlewares/not-found';
+import onError from './middlewares/on-error';
 
 const server = new OpenAPIHono();
 server.notFound(notFound);
+server.onError(onError);
 server.get('/', (c) => c.text('Hello, World!'));
 
 export default server;
