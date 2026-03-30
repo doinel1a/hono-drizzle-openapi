e84ff12b0b0ac97b8aa47f979baf6b1cebbeba8b
doinel1a <doinel1atanasiu@gmail.com>
2026-03-30 19:54:31 +0200

    fix: typescript errors

--- src/env.ts
+++ src/env.ts
@@ -11,15 +11,15 @@ export const env = createEnv({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development')
   },
   runtimeEnvStrict: {
-    PORT: process.env.PORT,
-    DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV
+    PORT: process.env['PORT'],
+    DATABASE_URL: process.env['DATABASE_URL'],
+    NODE_ENV: process.env['NODE_ENV']
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
    * This is especially useful for Docker builds.
    */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  skipValidation: !!process.env['SKIP_ENV_VALIDATION'],
   /**
    * Makes it so that empty strings are treated as undefined.
    * `SOME_VAR: z.string()` and `SOME_VAR=''` will throw an error.
