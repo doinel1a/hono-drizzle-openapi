0d9f6fe3d8fc5e0bbeca13af9c372a96b25a728a
doinel1a <doinel1atanasiu@gmail.com>
2026-04-03 23:56:04 +0200

    chore(auth): protect endpoints

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,35 @@
+import type { TServerBindings } from '../lib/types';
+
+import { cors as honoCors } from 'hono/cors';
+import { createMiddleware } from 'hono/factory';
+
+import { env } from '../env';
+import authConfig from '../lib/auth';
+import { UNAUTHORIZED_CODE } from '../lib/constants/http-status-codes';
+import { UNAUTHORIZED_PHRASE } from '../lib/constants/http-status-phrases';
+
+const auth = createMiddleware<TServerBindings>(async (c, next) => {
+  const session = await authConfig.api.getSession({ headers: c.req.raw.headers });
+  if (!session) {
+    c.set('session', null);
+    c.set('user', null);
+
+    return c.json({ message: UNAUTHORIZED_PHRASE }, UNAUTHORIZED_CODE);
+  }
+
+  c.set('session', session.session);
+  c.set('user', session.user);
+
+  return next();
+});
+
+export const cors = honoCors({
+  origin: env.ORIGIN,
+  allowHeaders: ['Content-Type', 'Authorization'],
+  allowMethods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
+  exposeHeaders: ['Content-Length'],
+  maxAge: 600,
+  credentials: true
+});
+
+export default auth;
--- src/routes/tasks/routes.ts
+++ src/routes/tasks/routes.ts
@@ -1,5 +1,6 @@
 import { createRoute, z } from '@hono/zod-openapi';
-import { insertTasksSchema, patchTodoSchema, selectTasksSchema } from '~/drizzle/schemas/tasks';
+import { insertTasksSchema, patchTasksSchema, selectTasksSchema } from '~/drizzle/schemas/tasks';
+import auth from '~/src/middlewares/auth';
 
 import {
   CONFLICT_CODE,
@@ -8,9 +9,13 @@ import {
   NO_CONTENT_CODE,
   NOT_FOUND_CODE,
   OK_CODE,
+  UNAUTHORIZED_CODE,
   UNPROCESSABLE_ENTITY_CODE
 } from '@/lib/constants/http-status-codes';
-import { INTERNAL_SERVER_ERROR_PHRASE } from '@/lib/constants/http-status-phrases';
+import {
+  INTERNAL_SERVER_ERROR_PHRASE,
+  UNAUTHORIZED_PHRASE
+} from '@/lib/constants/http-status-phrases';
 import createError from '@/lib/openapi/schemas/create-error';
 import idParamsSchema from '@/lib/openapi/schemas/id-params';
 import jsonContent from '@/lib/openapi/utils/json-content';
@@ -23,12 +28,15 @@ export const insert = createRoute({
   path: '/tasks',
   method: 'post',
   tags,
+  middleware: [auth],
+  security: [{ apiKeyCookie: [] }],
   request: {
     body: jsonContentRequired(insertTasksSchema, 'The task to insert')
   },
   responses: {
     [CREATED_CODE]: jsonContent(selectTasksSchema, 'The newly inserted task'),
     [CONFLICT_CODE]: jsonContent(z.object({ message: z.string() }), 'Database conflict(s)'),
+    [UNAUTHORIZED_CODE]: jsonContent(z.object({ message: z.string() }), UNAUTHORIZED_PHRASE),
     [UNPROCESSABLE_ENTITY_CODE]: jsonContent(
       z.union([
         z.object({ message: z.string() }), // Database
@@ -48,11 +56,14 @@ export const selectById = createRoute({
   path: '/tasks/{id}',
   method: 'get',
   tags,
+  middleware: [auth],
+  security: [{ apiKeyCookie: [] }],
   request: {
     params: idParamsSchema
   },
   responses: {
     [OK_CODE]: jsonContent(selectTasksSchema, 'The selected task'),
+    [UNAUTHORIZED_CODE]: jsonContent(z.object({ message: z.string() }), UNAUTHORIZED_PHRASE),
     [NOT_FOUND_CODE]: jsonContent(z.object({ message: z.string() }), 'Task not found'),
     [UNPROCESSABLE_ENTITY_CODE]: jsonContent(
       createError(idParamsSchema),
@@ -70,8 +81,11 @@ export const selectAll = createRoute({
   path: '/tasks',
   method: 'get',
   tags,
+  middleware: [auth],
+  security: [{ apiKeyCookie: [] }],
   responses: {
     [OK_CODE]: jsonContent(z.array(selectTasksSchema), 'All tasks'),
+    [UNAUTHORIZED_CODE]: jsonContent(z.object({ message: z.string() }), UNAUTHORIZED_PHRASE),
     [INTERNAL_SERVER_ERROR_CODE]: jsonContent(
       z.object({ message: z.string() }),
       INTERNAL_SERVER_ERROR_PHRASE
@@ -79,22 +93,25 @@ export const selectAll = createRoute({
   }
 });
 
-export type TPathRoute = typeof patch;
+export type TPatchRoute = typeof patch;
 export const patch = createRoute({
   path: '/tasks/{id}',
   method: 'patch',
   tags,
+  middleware: [auth],
+  security: [{ apiKeyCookie: [] }],
   request: {
     params: idParamsSchema,
-    body: jsonContentRequired(patchTodoSchema, 'The task to update')
+    body: jsonContentRequired(patchTasksSchema, 'The task to update')
   },
   responses: {
     [OK_CODE]: jsonContent(selectTasksSchema, 'The patched task'),
+    [UNAUTHORIZED_CODE]: jsonContent(z.object({ message: z.string() }), UNAUTHORIZED_PHRASE),
     [NOT_FOUND_CODE]: jsonContent(z.object({ message: z.string() }), 'Task not found'),
     [UNPROCESSABLE_ENTITY_CODE]: jsonContent(
       z.union([
         createError(idParamsSchema), // Parameter validation
-        createError(patchTodoSchema) // Input validation
+        createError(patchTasksSchema) // Input validation
       ]),
       'Parameter or input validation error(s)'
     ),
@@ -110,6 +127,8 @@ export const deleteById = createRoute({
   path: '/tasks/{id}',
   method: 'delete',
   tags,
+  middleware: [auth],
+  security: [{ apiKeyCookie: [] }],
   request: {
     params: idParamsSchema
   },
@@ -117,6 +136,7 @@ export const deleteById = createRoute({
     [NO_CONTENT_CODE]: {
       description: 'Task deleted successfully'
     },
+    [UNAUTHORIZED_CODE]: jsonContent(z.object({ message: z.string() }), UNAUTHORIZED_PHRASE),
     [NOT_FOUND_CODE]: jsonContent(z.object({ message: z.string() }), 'Task not found'),
     [UNPROCESSABLE_ENTITY_CODE]: jsonContent(
       createError(idParamsSchema),
