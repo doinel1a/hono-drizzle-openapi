fbdb251b6ec401c4722d73cd0869c9ecd1789721
doinel1a <doinel1atanasiu@gmail.com>
2026-04-03 23:52:43 +0200

    feat(auth): install & configure better-auth

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -3,12 +3,22 @@ import { Pool } from 'pg';
 
 import { env } from '@/env';
 
+import * as authSchemas from './schemas/auth';
 import { tasksSchema } from './schemas/tasks';
 
 const pool = new Pool({
   connectionString: env.DATABASE_URL
 });
 
-const db = drizzle({ client: pool, schema: { tasks: tasksSchema } });
+const db = drizzle({
+  client: pool,
+  schema: {
+    users: authSchemas.usersSchema,
+    sessions: authSchemas.sessionsSchema,
+    accounts: authSchemas.accountsSchema,
+    verifications: authSchemas.verificationsSchema,
+    tasks: tasksSchema
+  }
+});
 
 export default db;
