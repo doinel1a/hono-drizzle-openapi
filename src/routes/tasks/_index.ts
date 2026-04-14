fdbd424bdf274e873554325a47c0d37166bf1a6e
doinel1a <doinel1atanasiu@gmail.com>
2026-04-14 22:01:26 +0200

    refactor(routes/tasks/*): db as dependency injection

--- src/routes/tasks/_index.ts
+++ src/routes/tasks/_index.ts
@@ -1,13 +1,17 @@
+import type { TDatabase } from '@/lib/types';
+
 import { createRouter } from '@/lib/init-server';
 
-import * as handlers from './handlers';
+import initHandlers from './handlers';
 import * as routes from './routes';
 
-const router = createRouter()
-  .openapi(routes.insert, handlers.insert)
-  .openapi(routes.selectAll, handlers.selectAll)
-  .openapi(routes.selectById, handlers.selectById)
-  .openapi(routes.patch, handlers.patch)
-  .openapi(routes.deleteById, handlers.deleteById);
+export default function initRoutes(db: TDatabase) {
+  const handlers = initHandlers(db);
 
-export default router;
+  return createRouter()
+    .openapi(routes.insert, handlers.insert)
+    .openapi(routes.selectAll, handlers.selectAll)
+    .openapi(routes.selectById, handlers.selectById)
+    .openapi(routes.patch, handlers.patch)
+    .openapi(routes.deleteById, handlers.deleteById);
+}
