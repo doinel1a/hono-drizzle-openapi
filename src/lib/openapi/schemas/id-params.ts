f8c71d6e51c0e6ea1d49e719fe9ce2aa4f126dea
doinel1a <doinel1atanasiu@gmail.com>
2026-04-15 12:55:55 +0200

    fix(lib/openapi/schemas/id-params): ensure id is a positive integer number

--- src/lib/openapi/schemas/id-params.ts
+++ src/lib/openapi/schemas/id-params.ts
@@ -1,15 +1,19 @@
 import { z } from '@hono/zod-openapi';
 
 const idParametersSchema = z.object({
-  id: z.coerce.number().openapi({
-    param: {
-      name: 'id',
-      in: 'path',
-      required: true
-    },
-    required: ['id'],
-    example: 123
-  })
+  id: z.coerce
+    .number()
+    .int()
+    .positive()
+    .openapi({
+      param: {
+        name: 'id',
+        in: 'path',
+        required: true
+      },
+      required: ['id'],
+      example: 123
+    })
 });
 
 export default idParametersSchema;
