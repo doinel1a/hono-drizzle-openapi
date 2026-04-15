709b7244ee825b59cad321d1b8b3308e2ddadb10
doinel1a <doinel1atanasiu@gmail.com>
2026-04-15 13:04:39 +0200

    chore(lint-staged): run tests for only files that have them

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import path from 'node:path';
 
 const tsc = () => 'tsc --noEmit';
@@ -8,9 +9,31 @@ const eslint = (fileNames: string[]) =>
 const prettier = (fileNames: string[]) =>
   `prettier --write ${fileNames.map((fileName) => path.relative(process.cwd(), fileName)).join(' ')} --cache`;
 
+const vitest = (fileNames: string[]) => {
+  const testFiles = fileNames
+    .flatMap((fileName) => {
+      const relative = path.relative(process.cwd(), fileName);
+      const withoutSource = relative.replace(/^src[\\/]/, '');
+      const withoutExtension = withoutSource.replace(/\.ts$/, '');
+
+      return [
+        path.join('tests', 'unit', `${withoutExtension}.test.ts`),
+        path.join('tests', 'integration', `${withoutExtension}.test.ts`)
+      ];
+    })
+    .filter((testFile) => fs.existsSync(testFile));
+
+  if (testFiles.length === 0) {
+    return [];
+  }
+
+  return [`vitest run ${testFiles.map((f) => path.normalize(f)).join(' ')}`];
+};
+
 const config = {
   '*.{ts,mts,cts}': [tsc],
-  '*.{js,mjs,cjs,ts,mts,cts}': [eslint, prettier]
+  '*.{js,mjs,cjs,ts,mts,cts}': [eslint, prettier],
+  'src/**/*.ts': [vitest]
 };
 
 export default config;
